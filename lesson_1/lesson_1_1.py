7069e575afa02fa0dc7ea08271413a4dabb57221
AlekseiAleksin <aleksinq@gmail.com>
2025-10-27 19:23:22 +0300

    homework 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,27 @@
-print("Привет, мир")
+# a = 7
+# str_1 = "легко в ИТ"
+# b = a
+# a = 8
+# print(b)
+
+# a = b = c = 0
+# a, b, c=0, 0, 0
+
+# a,b=8,10
+# a,b=b,a
+# print(a)
+# print(b)
+
+# print(type(a))
+# print(type(str_1))
+
+# message = "Привет"
+# count = 0
+# Count = 0
+
+a=c=4
+b,d="строка","вторая строка"
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
